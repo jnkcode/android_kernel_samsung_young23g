e6301326bd1b7aa7714f782da65675fd791fe691
cakl <anggrekkelam@gmail.com>
2017-04-11 14:16:22 +0700

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 PREEMPT Tue Apr 11 13:54:38 WIB 2017"
+#define LINUX_COMPILE_BY "cakl"
+#define LINUX_COMPILE_HOST "cakl-OEM"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
