33202d19fbd78d96fe4a879d50d027bea550926b
cakl <anggrekkelam@gmail.com>
2017-04-18 03:06:08 +0700

    FROMLIST: mm: mmap: Add new /proc tunable for mmap_base ASLR. FROMLIST: arm: mm: support ARCH_MMAP_RND_BITS. defconfig: young23g-native_hw03_defconfig: enable ARCH_MMAP_RND_BITS.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Tue Apr 11 22:48:14 WIB 2017"
+#define UTS_VERSION "#5 PREEMPT Tue Apr 18 02:39:17 WIB 2017"
 #define LINUX_COMPILE_BY "cakl"
 #define LINUX_COMPILE_HOST "cakl-OEM"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
